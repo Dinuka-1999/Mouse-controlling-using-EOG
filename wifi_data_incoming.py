d30d098301acdc9b3bcdbb451ee6f2aa7df82bbc
Dinuka-1999 <81279517+Dinuka-1999@users.noreply.github.com>
2023-07-26 01:11:47 +0530

    added the decision taking algorithm

--- wifi_data_incoming.py
+++ wifi_data_incoming.py
@@ -8,18 +8,19 @@ import pyqtgraph as pg
 import threading
 import time
 from scipy import signal
-from scipy import ndimage
-
+import pyautogui
 
 UDP_IP = "172.20.10.11" # The IP that is printed in the serial monitor from the ESP32
 SHARED_UDP_PORT = 4210
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
 sock.connect((UDP_IP, SHARED_UDP_PORT))
-
+trigger_sample=499
+hori_Channel=2
+verti_channel=3
 # Create the PyQtGraph application
 app = QApplication([])
 win = pg.GraphicsLayoutWidget(show=True)
-show = [2,4]
+show = [2,3,4]
 plot=[]
 orig_plot = []
 baseLine_plot=[]
@@ -27,7 +28,7 @@ decision_plot=[]
 
 for j,i in enumerate(show):
     plot.append(win.addPlot(title=f'Channel {i}'))
-    # orig_plot.append(win.addPlot(title=f'Original {i}'))
+    orig_plot.append(win.addPlot(title=f'Original {i}'))
     decision_plot.append(win.addPlot(title=f'Decision {i}'))
     # baseLine_plot.append(win.addPlot(title=f'base_Line {i}'))
     win.nextRow()
@@ -36,15 +37,16 @@ filtered_curve = []
 orig_curve = []
 baseLine_curve=[]
 decision_curve=[]
-
+upper_thresh_curve=[]
+lower_thresh_curve=[]
 color = ['b','g','r','c','b','g','r','c','b']
 for i in range(len(show)):
     filtered_curve.append(plot[i].plot(pen='g'))
-    orig_curve.append(plot[i].plot(pen='b'))
+    orig_curve.append(orig_plot[i].plot(pen='b'))
     baseLine_curve.append(plot[i].plot(pen='r'))
     decision_curve.append(decision_plot[i].plot(pen='r'))
-    # baseLine_curve.append(plot[i].plot(pen='r'))
-
+    upper_thresh_curve.append(plot[i].plot(pen='y'))
+    lower_thresh_curve.append(plot[i].plot(pen='y'))
 xdata = np.arange(1000)  # Number of data points to display on the plot
 ydata = np.zeros((9,1000))
 num_samples = 0
@@ -71,10 +73,12 @@ def loop():
 
 def update_plot():
     for i,j in enumerate(show):
+        orig_curve[i].setData(y=ydata_avg[j])
         filtered_curve[i].setData(y=filtered[i])
-        orig_curve[i].setData(y=ydata[j])
         baseLine_curve[i].setData(y=baseLine_filtered[i])
         decision_curve[i].setData(y=decision_arr[i])
+        upper_thresh_curve[i].setData(y=upper_thresh[i])
+        lower_thresh_curve[i].setData(y=lower_thresh[i])
 
 def second_timer():
     global rate,num_samples
@@ -84,84 +88,89 @@ def second_timer():
         num_samples = 0
         # print(rate)
 
-difference=np.zeros((1,1000))
+def decision_take(decision,channel):
+    decision_sample=decision[trigger_sample]
+    if decision_sample!=0:
+        # print(decision_sample)
+        edges=np.diff(decision)
+        # edges=np.zeros((1000))
+        split_edges=edges[trigger_sample-9:trigger_sample+250]
+        array_seq=split_edges[split_edges!=0].tolist()
 
-# def classify_signal():
-#     global threshold1 , threshold2,decision_arr
-#     decision_arr=np.zeros((2,1000))
-#     difference=np.zeros((2,1000))
-    
-#     while True:
-#         # for i,j in enumerate(show):
-#         #     difference[i]=filtered[j]-baseLine_filtered[i]
+        print(array_seq)
+        if channel==verti_channel:
+            if (array_seq==[-1,1,1,-1] or array_seq==[-1,1]):
+                print("Eye down")
+                #pyautogui.moveRel(0,-50, duration = 0.5)
+            elif array_seq==[1,-1,-1,1]:
+                print("Eye up")
+                #pyautogui.moveRel(0, 50, duration = 1)
+            elif array_seq==[1,-1]:
+                print("Blink")
+            else:
+                print("try again")
 
-#         difference=filtered-baseLine_filtered
-#         arr1=(difference>1000).astype(int)
-#         arr2=-(difference<-1000).astype(int)
-#         print(arr1)
+        elif channel==hori_Channel:
+            if (array_seq==[-1,1,1,-1] or array_seq==[-1,1]):
+                print("Eye left")
+               # pyautogui.moveRel(-50,0, duration = 0.5)
+            elif (array_seq==[1,-1,-1,1] or array_seq==[1,-1]):
+                print("Eye right")
+               # pyautogui.moveRel(50,0, duration = 0.5)
+            else:
+                print("try again")
 
-#         # if arr1[-1]==True:
-#         #     decision_arr[-1]=1 
-#         # elif arr2[-1]==True:
-#         #     decision_arr[-1]=-1
-#         # else:
-#         #     decision_arr[-1]=0
-#         decision_arr=arr1+arr2
-#         # if len(decision_arr)==3:
-#         #     if decision_arr==[1,0,0]:
-#         #         print("blink")
-#         #     elif decision_arr==[1,0,-1]:
-#         #         print("up")
-#         #     elif decision_arr==[-1,0,1]:
-#         #         print("down")
-#         #     decision_arr=[]
+        time.sleep(1)
 
+def classifier():
+    time.sleep(1)
+    while True:
+        time.sleep(0.001)
+        for r,i in enumerate(show):
+            decision_take(decision_arr[r],i)
 
 def filter():
-    global rate, filtered, baseLine_filtered,decision_arr
+    global rate, filtered, baseLine_filtered,decision_arr,ydata_avg,upper_thresh,lower_thresh,big_difference
     # bandpass = signal.butter(10, [0.2,40], 'bandpass', fs=rate, output='sos')
-
+    upper_thresh=np.zeros((len(show),1000))
+    lower_thresh=np.zeros((len(show),1000))
+    big_difference=np.zeros((len(show),10000))
     while True:
-        # highpass = signal.butter(10, 0.5, 'hp', fs=rate, output='sos')
+    
         lowpass = signal.butter(30, 80, 'lp', fs=rate, output='sos')
         bandstop = signal.butter(10, [48,52], 'bandstop', fs=rate, output='sos')
-        # baseLine_filter=signal.butter(10, 0.2, 'lp', fs=rate, output='sos')
-        # ha,hb = signal.butter(10, 0.2, 'hp', fs=rate)
-        # la,lb = signal.butter(10, 40, 'lp', fs=rate)
-
-        # filtered = signal.sosfilt(bandpass, ydata)
-
-        # filtered = signal.sosfilt(highpass, ydata)
-        # remove average of ydata
-        # print(np.mean(ydata,axis=1))
-        # ydata_avg = ydata - np.mean(ydata,axis=1).reshape(-1,1)
-        # print(ydata_avg.shape)
-        # filtered1 = signal.sosfilt(bandstop, ydata_avg)
-        # filtered = signal.sosfilt(lowpass, filtered1)
-        filtered=np.zeros((2,ydata.shape[1]))
+        
+        ydata_avg = ydata - np.mean(ydata,axis=1).reshape(-1,1)
+        
+        filtered1=np.zeros((len(show),ydata.shape[1]))
         for r,i in enumerate(show):
-            filtered[r]=signal.medfilt(ydata[i], kernel_size=9)
+            filtered1[r] = signal.sosfilt(bandstop, ydata_avg[i])
+
+        filtered2=np.zeros((len(show),ydata.shape[1]))
+        for r,i in enumerate(show):
+            filtered2[r] = signal.sosfilt(lowpass, filtered1[r])
             
-        baseLine_filtered=np.zeros((2,ydata.shape[1]))
+        filtered=np.zeros((len(show),ydata.shape[1]))
         for r,i in enumerate(show):
-            baseLine_filtered[r]=signal.medfilt(ydata[i], kernel_size=151)
-        # baseLine_filtered=signal.sosfilt(baseLine_filter, filtered)
+            filtered[r]=signal.medfilt(filtered2[r], kernel_size=9)
+            
+        baseLine_filtered=np.zeros((len(show),ydata.shape[1]))
+        for r,i in enumerate(show):
+            baseLine_filtered[r]=signal.medfilt(filtered[r], kernel_size=151)
 
-        # filtered1 = signal.filtfilt(ha,hb,ydata)
-        # filtered = signal.filtfilt(la,lb,filtered1)
         difference=filtered-baseLine_filtered
-        threshold=2000
-        arr1=(difference>threshold).astype(int)
-        arr2=-(difference<-threshold).astype(int)
+        sd=np.std(big_difference,axis=1)
+        k=3
+        samp=baseLine_filtered[:,trigger_sample]
 
-        # if arr1[-1]==True:
-        #     decision_arr[-1]=1 
-        # elif arr2[-1]==True:
-        #     decision_arr[-1]=-1
-        # else:
-        #     decision_arr[-1]=0
+        upper_thresh[:,:]=baseLine_filtered+(k*sd).reshape(len(show),1)
+        lower_thresh[:,:]=baseLine_filtered+(-k*sd).reshape(len(show),1)
+        arr1=(difference>(samp+k*sd).reshape(len(show),1)).astype(int)
+        arr2=-(difference<(samp-k*sd).reshape(len(show),1)).astype(int)
+
         decision_arr=arr1+arr2
-
+        big_difference[:,:-1]=big_difference[:,1:]
+        big_difference[:,-1]=difference[:,-1]
 # Set up a timer to update the plot
 timer = QtCore.QTimer()
 timer.timeout.connect(update_plot)
@@ -175,7 +184,7 @@ if __name__ == "__main__":
     t2.start()
     t3 = threading.Thread(target=filter)
     t3.start()
-    # t4 = threading.Thread(target=classify_signal)
-    # t4.start()
+    t4 = threading.Thread(target=classifier)
+    t4.start()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QApplication.instance().exec_()
